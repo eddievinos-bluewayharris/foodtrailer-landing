d8056b575906f03d253704eed9768d3881d43d6d
Eddie Vinos <eddievinos@gmail.com>
2025-10-14 17:07:16 -0600

    Swiper: navegación, paginación, teclado y loop

--- src/ModelGallery.jsx
+++ src/ModelGallery.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Keyboard, A11y } from 'swiper/modules';
+
+// Estilos básicos de Swiper + módulos
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 export default function ModelGallery({ images = [] }) {
   return (
-    <Swiper spaceBetween={10} slidesPerView={1} style={{ borderRadius: 12 }}>
+    <Swiper
+      modules={[Navigation, Pagination, Keyboard, A11y]}
+      spaceBetween={10}
+      slidesPerView={1}
+      navigation           // ← flechas izquierda/derecha
+      pagination={{ clickable: true }} // ← bullets clicables
+      keyboard={{ enabled: true }}     // ← teclas ← →
+      loop={images.length > 1}         // ← loop si hay >1 imagen
+      style={{ borderRadius: 12 }}
+    >
       {images.map((src, idx) => (
         <SwiperSlide key={idx}>
           <img
